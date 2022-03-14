213582bba373e8991c7f347883f629ddd7d238bc
1234edg <86652346+1234edg@users.noreply.github.com>
2022-03-14 16:03:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,13 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAkp3gQdHbMPD5pCfHbBzIIgFpdCJXD5KM",
-  authDomain: "wily-app-v2.firebaseapp.com",
-  projectId: "wily-app-v2",
-  storageBucket: "wily-app-v2.appspot.com",
-  messagingSenderId: "772559744213",
-  appId: "1:772559744213:web:9417b879882f231175065c",
-  measurementId: "G-H8Y352W6Z2"
+  apiKey: "AIzaSyDJPDBsEhDaaPBf8iJ5dekxZ5n_lcSkJ-0",
+  authDomain: "c77sa-69aac.firebaseapp.com",
+  projectId: "c77sa-69aac",
+  storageBucket: "c77sa-69aac.appspot.com",
+  messagingSenderId: "860933350485",
+  appId: "1:860933350485:web:464c17dc8ba169c10d60dc"
 };
-
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.firestore();
